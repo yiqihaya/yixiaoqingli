b0c681c55e9d1d85f0b5747bdb10daabc842522c
2720509126 <2720509126@qq.com>
2026-06-07 14:00:06 +0800

    📦 部署：将 dist 内容放到仓库根目录

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Program%20Files/Git/yixiaoqingli/sw.js', { scope: '/Program%20Files/Git/yixiaoqingli/' })})}
